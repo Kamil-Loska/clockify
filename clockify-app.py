2b6b322a777ad785aeb001319c2fada87c5028a6
Kamil-Loska <112934710+Kamil-Loska@users.noreply.github.com>
2023-05-24 18:03:26 +0200

    Allowing multiple users to raport

--- clockify-app.py
+++ clockify-app.py
@@ -2,14 +2,22 @@ import requests
 import datetime
 import csv
 import os
+import configparser
 
 class ClockifyApp:
 
     def __init__(self):
-        self.API_KEY = 'API_KEY'
-        self.WORKSPACE_ID = 'WORKSPACE_ID'
-        self.USER_ID = 'USER_ID'
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+
+        clockify_section = self.config['Clockify']
+        self.API_KEY = clockify_section.get('API_KEY')
+        self.WORKSPACE_ID = clockify_section.get('WORKSPACE_ID')
+        self.USER_ID = clockify_section.get('USER_ID')
         self.BASE_URL = 'https://api.clockify.me/api/v1/'
+
+        self.users = self.get_users_from_config()
+
     def get_time_entries_per_user(self, start_date, end_date):
 
         endpoint = f'workspaces/{self.WORKSPACE_ID}/user/{self.USER_ID}/time-entries'
@@ -32,30 +40,44 @@ class ClockifyApp:
 
         return all_data
 
+    def get_users_from_config(self):
+        users = {}
+        for section in self.config.sections():
+            if section.startswith('User'):
+                user_id = self.config.get(section, 'ID')
+                api_key = self.config.get(section, 'API_KEY')
+                users[user_id] = api_key
+
+        return users
+
     def generate_raport(self, date_from='', date_to=''):
         if not self.validate_date_format(date_from, date_to):
             print("Invalid date format. Please provide dates in the format 'YYYY-MM-DD'. ")
             return
-        get_data = self.get_time_entries_per_user(date_from, date_to)
-        get_user_id, get_user_name = self.get_user_data()
 
-        for data in get_data:
-            create_date = data['timeInterval']['start'][:10]
-            duration = data['timeInterval']['duration']
-            name = data['description']
-            if name == "":
-                name = "In progress..."
+        for user_id, api_key in self.users.items():
+            self.API_KEY = api_key
+            self.USER_ID = user_id
+            get_data = self.get_time_entries_per_user(date_from, date_to)
+            get_user_id, get_user_name = self.get_user_data()
 
-            if data['userId'] == get_user_id and date_from <= create_date <= date_to:
-                member_name = get_user_name
+            for data in get_data:
+                create_date = data['timeInterval']['start'][:10]
+                duration = data['timeInterval']['duration']
+                name = data['description']
+                if name == "":
+                    name = "In progress..."
 
-                report_data = {
-                    'Imię i nazwisko': " ".join(member_name.split(" ")[::-1]),
-                    'Data': create_date,
-                    'Czas trwania': self.format_duration(duration),
-                    'Opis zadania': name,
-                }
-                print(report_data)
+                if data['userId'] == get_user_id and date_from <= create_date <= date_to:
+                    member_name = get_user_name
+
+                    report_data = {
+                        'Imię i nazwisko': " ".join(member_name.split(" ")[::-1]),
+                        'Data': create_date,
+                        'Czas trwania': self.format_duration(duration),
+                        'Opis zadania': name,
+                    }
+                    print(report_data)
 
     def format_duration(self, duration):
         if duration != None:
@@ -91,7 +113,6 @@ class ClockifyApp:
     def get_user_data(self):
         endpoint = f'user'
         get_user_data = self.send_get_request(endpoint)
-
         return get_user_data['id'], get_user_data['name']
 
     def validate_date_format(self, first_date, second_date):
