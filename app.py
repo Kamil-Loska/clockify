08d85bcf24934208c5c9e2a752453f38437a29d2
Kamil-Loska <112934710+Kamil-Loska@users.noreply.github.com>
2023-05-24 11:09:39 +0200

    Refactor script for generating Clockify report

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import sys
 from main import ClockifyApp
 
-if __name__ == '__main__':
+def main():
     clockify = ClockifyApp()
-    date_from = sys.argv[1]
-    date_to = sys.argv[2]
     if len(sys.argv) != 3:
         print("Invalid number of arguments. Please provide two dates as arguments")
+        return
+    date_from = sys.argv[1]
+    date_to = sys.argv[2]
     if not clockify.validate_date_format(date_from, date_to):
         print("Invalid date format. Please provide dates in the format 'YYYY-MM-DD'. ")
-    else:
-        clockify.generate_raport(date_from, date_to)
+        return
+    clockify.generate_raport(date_from, date_to)
+
+if __name__ == '__main__':
+    main()
